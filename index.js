8de81b28b3d6dcbca0b8e73fab95a5f70575a714
Soldy <4786022+Soldy@users.noreply.github.com>
2021-07-28 21:41:46 +0100

    first tags
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+const loremBase = function(){
+    const _single = ['a','e','i','o','u','q'];
+    const _multi = ['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','x','y','z'];
+    const _single_double = ['ei','ie','io','iu','oi','ua','ui']
+    const _multi_double = [
+        'cc','ch','ct','cr','dd',
+        'dh','dm','dr','dv','ff',
+        'fr',t'gg',
+        'gn','gh','ll','lv','ls',
+        'mn','mp','nd','nm','ps',
+        'pg','rd','rt','sp','st',
+        'ss','tr','tp'
+    ];
+    
+}
+
+
